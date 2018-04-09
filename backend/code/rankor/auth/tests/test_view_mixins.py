60f6c87dcb2d3fbd373ce4ec89c5823622f6738d
Dominik Dlugajczyk <d.dlugajczyk@clearcode.cc>
2018-04-09 21:37:31 +0200

    Make AuthMixin.get_user to respect JWT token.

--- backend/code/rankor/auth/tests/test_view_mixins.py
+++ backend/code/rankor/auth/tests/test_view_mixins.py
@@ -28,10 +28,20 @@ class TestAuthMixin(object):
             yield mock
 
     @fixture
+    def mdecode_jwt(self):
+        with patch('rankor.auth.view_mixins.decode_jwt') as mock:
+            yield mock
+
+    @fixture
     def mrequest(self, mixin):
         mrequest = MagicMock()
+        mrequest.headers = {}
         mixin.request = mrequest
         return mrequest
+
+    @fixture
+    def get_user(self, mixin):
+        return undecorated(mixin.get_user)
 
     def test_get_user(
             self,
@@ -40,19 +50,43 @@ class TestAuthMixin(object):
             mauthenticated_userid,
             muser_query,
             mrequest,
+            get_user,
     ):
         """
         .get_user should return authenticated user from the database.
         """
         mquery = muser_query.return_value
-        fun = undecorated(mixin.get_user)
 
-        assert fun(mixin, dbsession=mdb) == mquery.get_by_id.return_value
+        assert get_user(mixin, dbsession=mdb) == mquery.get_by_id.return_value
 
         muser_query.assert_called_once_with(mdb)
         mquery.get_by_id.assert_called_once_with(
             mauthenticated_userid.return_value)
         mauthenticated_userid.assert_called_once_with(mrequest)
+
+    def test_get_user_on_proper_jwt(
+            self,
+            mixin,
+            mdb,
+            muser_query,
+            mrequest,
+            get_user,
+            mdecode_jwt,
+    ):
+        """
+        .get_user should return authenticated user when proper jwt provided
+        """
+        mquery = muser_query.return_value
+        mrequest.headers['JWT'] = 'proper.jwt.token'
+        mdecode_jwt.return_value = {
+            'uuid': 'uuid1'
+        }
+
+        assert get_user(mixin, dbsession=mdb) == mquery.get_by_uuid.return_value
+
+        muser_query.assert_called_once_with(mdb)
+        mquery.get_by_uuid.assert_called_once_with('uuid1')
+        mdecode_jwt.assert_called_once_with('proper.jwt.token')
 
     @mark.parametrize('return_value, is_auth', [
         [None, False],
--- backend/code/rankor/auth/view_mixins.py
+++ backend/code/rankor/auth/view_mixins.py
@@ -3,6 +3,7 @@ from sapp.decorators import WithContext
 
 from rankor import app
 from rankor.auth.drivers import UserQuery
+from rankor.auth.jwt import decode_jwt
 
 
 class AuthMixin(object):
@@ -10,7 +11,13 @@ class AuthMixin(object):
     def get_user(self, dbsession):
         # TODO: think about caching this per request or context?
         user_rd = UserQuery(dbsession)
-        return user_rd.get_by_id(self.get_user_id())
+
+        jwt = self.request.headers.get('JWT')
+        if jwt:
+            payload = decode_jwt(jwt)
+            return user_rd.get_by_uuid(payload['uuid'])
+        else:
+            return user_rd.get_by_id(self.get_user_id())
 
     def get_user_id(self):
         return authenticated_userid(self.request)
